76ae86fa6203bb20a8a5c4adf213f1cff3cfccb4
farisa <farisa.hafida@gmail.com>
2019-09-17 13:06:38 +0700

    deploy

--- src/App.js
+++ src/App.js
@@ -82,7 +82,7 @@ class App extends Component {
   onButtonSubmit = () => {
     console.log('click');
     this.setState({imageUrl: this.state.input});
-    fetch('http://localhost:3000/imageurl', {
+    fetch('https://fierce-fjord-66099.herokuapp.com/imageurl', {
       method: 'post',
       headers: {'Content-Type': 'application/json'},
       body: JSON.stringify({
@@ -92,7 +92,7 @@ class App extends Component {
       .then(response => response.json())
       .then(response => {
         if (response) {
-          fetch('http://localhost:3000/image', {
+          fetch('https://fierce-fjord-66099.herokuapp.com/image', {
             method: 'put',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -17,7 +17,7 @@ class SignIn extends Component {
     }
 
     onSubmitSignIn = () => {
-        fetch('http://localhost:3000/signin', {
+        fetch('https://fierce-fjord-66099.herokuapp.com/signin', {
             method: 'post',
             headers: {'Content-Type': 'application/json'},
             body: JSON.stringify({
